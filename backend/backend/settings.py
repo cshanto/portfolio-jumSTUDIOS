e97669363b53658074f351fbdc020cb41e4e1670
cshanto39 <cshanto39@gmail.com>
2025-08-23 18:40:39 +0530

    Day 2: Created apps and configured DB + admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Our custom apps
+    'users',
+    'services',
+    'orders',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
